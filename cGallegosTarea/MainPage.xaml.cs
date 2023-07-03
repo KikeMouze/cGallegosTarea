95fd60f86bf84a69acac03ddc2a33859467623c0
gtchr <gtchr@DESKTOP-JAB6IJU>
2023-07-03 11:14:54 -0500

    Agregar archivos de proyecto.

---
+++ cGallegosTarea/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace cGallegosTarea
+{
+    public partial class MainPage : ContentPage
+    {
+        private string Url = "http://10.1.0.32/promocion/post.php";
+        private HttpClient cliente = new HttpClient();
+        private ObservableCollection<Promocion> post;
+        public MainPage()
+        {
+            InitializeComponent();
+            ObtenerDatos();
+
+        }
+        public async void ObtenerDatos()
+        {
+            var contenido = await cliente.GetStringAsync(Url);
+            List<Promocion> listaPost = JsonConvert.DeserializeObject<List<Promocion>>(contenido);
+            post = new ObservableCollection<Promocion>(listaPost);
+            listaEstudiantes.ItemsSource = post;
+        }
+        private async void btnMonstrar_Clicked(object sender, EventArgs e)
+        {
+            var contenido = await cliente.GetStringAsync(Url);
+            List<Promocion> listaPost = JsonConvert.DeserializeObject<List<Promocion>>(contenido);
+            post = new ObservableCollection<Promocion>(listaPost);
+            listaEstudiantes.ItemsSource = post;
+        }
+    }
+}
---
+++ cGallegosTarea/Promocion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cGallegosTarea
+{
+    internal class Promocion
+    {
+        public int id_producto { set; get; }
+        public String nombre_producto { set; get; }
+        public String descripcion_producto { set; get; }
+        public String precio_promocion { set; get; }
+        public String condicion_promocion { set; get; }
+        public String codigo_promocion { set; get; }
+        public String producto_relacionado { set; get; }   
+
+    }
+}
